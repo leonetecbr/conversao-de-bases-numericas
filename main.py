c7b2156ea5e297b6442d64b9cca827a8f666c47f
Leone Oliveira <falecom@leone.tec.br>
2022-03-30 15:53:14 -0300

    Adiciona novas opções de conversão

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 from utils import octal, convert, hexadecimal
 
+from_type = ''
+from_types = ['decimal', 'octal', 'binário']
+while not (from_type in from_types):
+    from_type = input('Converter de decimal, octal ou de binário? ').lower().strip()
+
 num = ''
 while not num.isdigit():
-    num = input('Insira o número binário: ').replace(' ', '')
+    num = input(f'Insira o número {from_type}: ').replace(' ', '')
 
 to = ''
-while not (to == 'decimal' or to == 'octal' or to == 'hexadecimal'):
-    to = input('Converter para decimal, octal ou para hexadecimal? ').lower().strip()
+to_type = ['decimal', 'octal', 'hexadecimal', 'binário']
+to_type.remove(from_type)
+while not (to in to_type):
+    to = input(f'Converter para {to_type[0]}, {to_type[1]} ou para {to_type[2]}: ').lower().strip()
 
-
-if to == 'octal':
-    print(f'O número binário {num} é equivalente ao octal {octal(num)}')
-elif to == 'decimal':
-    print(f'O número binário {num} é equivalente ao decimal {convert(num)}')
+if from_type == 'binário':
+    if to == 'octal':
+        print(f'O número binário {num} é equivalente ao octal {octal(num)}')
+    elif to == 'decimal':
+        print(f'O número binário {num} é equivalente ao decimal {convert(num)}')
+    else:
+        print(f'O número binário {num} é equivalente ao hexadecimal {hexadecimal(num)}')
+elif from_type == 'octal':
+    # To do
+    print('Função não definida!')
+elif from_type == 'decimal':
+    # To do
+    print('Função não definida!')
 else:
-    print(f'O número binário {num} é equivalente ao hexadecimal {hexadecimal(num)}')
+    # hexadecimal
+    print('Função não definida!')
